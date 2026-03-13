0f4ed8dc61c6a86e5eff263827d179140e1be9a4
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2026-03-13 13:25:15 -0700

    add admin button to profile

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -10,6 +10,9 @@
 <div class="mx-auto mt-26 flex w-2/3 flex-col text-stone-400">
 	<div class="flex justify-between">
 		<h1 class="text-2xl text-stone-300">{data.user.username}</h1>
+		{#if data.user.is_admin}
+			<a href="/admin">open admin panel</a>
+		{/if}
 		<button
 			class="transition-colors duration-300 hover:cursor-pointer hover:text-stone-300 hover:underline"
 			onclick={logout}>logout</button
