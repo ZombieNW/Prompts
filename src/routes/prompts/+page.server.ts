c00b0fd33d7dccccf9ac410139eb8312d9bac44a
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2026-03-13 13:27:13 -0700

    Don't require auth on prompts page

--- src/routes/prompts/+page.server.ts
+++ src/routes/prompts/+page.server.ts
@@ -3,7 +3,7 @@ import { getTodaysPrompt, getPastPrompts } from '$lib/server/prompts';
 import { getResponsesForPrompt, getUserResponse } from '$lib/server/responses.js';
 
 export const load = async (event) => {
-	const user = requireAuth(event);
+	const user = event.locals.user;
 	const todaysPrompt = getTodaysPrompt();
 	const prompts = getPastPrompts();
 
