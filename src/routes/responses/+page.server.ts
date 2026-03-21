ed921660850e1ad867d20d99ca0375a70ff43d00
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2026-03-21 14:41:12 -0700

    Revise response page

--- src/routes/responses/+page.server.ts
+++ src/routes/responses/+page.server.ts
@@ -11,6 +11,7 @@ export const load = async (event) => {
 	return {
 		user,
 		responses,
-		userResponse
+		userResponse,
+		todaysPrompt
 	};
 };
--- src/routes/responses/+page.svelte
+++ src/routes/responses/+page.svelte
@@ -1,11 +1,16 @@
 <script>
 	import Response from '$lib/components/ResponseBox.svelte';
+	import PromptBox from '$lib/components/PromptBox.svelte';
 
 	let { data } = $props();
 </script>
 
 <div class="mx-auto mt-26 flex w-2/3 flex-col text-stone-400">
-	<h1>Today's Responses</h1>
+	<h1 class="text-2xl text-stone-300">Responses</h1>
+	<hr class="mb-6 border-stone-700" />
+
+	<h1>Today's Prompt</h1>
+	<PromptBox prompt={data.todaysPrompt} />
 	<hr class="mb-6 border-stone-700" />
 
 	{#if data.userResponse === null}
@@ -14,14 +19,20 @@
 				>Submit your own response</a
 			> before viewing other's responses.
 		</p>
+		<hr class="mb-6 border-stone-700" />
 	{:else}
 		<h1>Your Response</h1>
 		<Response response={data.userResponse} />
+		<hr class="mb-6 border-stone-700" />
+
 		<h1>Public Responses</h1>
 		<ul class="mb-6">
 			{#each data.responses as response}
-				<li><Response {response} /></li>
+				{#if response.id !== data.userResponse.id}
+					<li><Response {response} /></li>
+				{/if}
 			{/each}
 		</ul>
+		<hr class="mb-6 border-stone-700" />
 	{/if}
 </div>
